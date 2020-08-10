9fc5bd9fc9b57b43c06c2a86ac0e902209269af4
mohamed eldemellawy <demeshdemesh@yahoo.com>
2020-08-10 03:25:22 +0200

    FIRST COMMIT

---
+++ uart_main.c
@@ -0,0 +1,41 @@
+/*
+ * File:   uart_main.c
+ * Author: ELDEMELAWY
+ *
+ * Created on August 10, 2020, 2:27 AM
+ */
+
+#include <avr/io.h>
+
+void UART_INTI () // USART INITAILZATION FUNCATION 
+{
+    UCSRB |= (1<<TXEN);// TRANSMITTER ENABLE 
+    UCSRB |= (1<<RXEN); // RECEIVER ENABLE 
+    UCSRC |= (1<<URSEL); // REGISER SELECTOR 
+    UCSRC |= (1<<UCSZ1);
+    UCSRC |= (1<<UCSZ0);// IT MEANS THAT I WILL USE 8 BIT IN FRAME 
+    UCSRB |= (1<<RXCIE) ; // RECIVE INTERRUPT 
+    UBRRL = 51; // BUDE RATE 9600 MBPS 
+}
+
+
+void UART_TRANSMITCHAR (char data)
+{
+    while (!(UCSRA & (1<<UDRE))) // (pooling) wait register to be empty 
+    {
+        UDR = data ;
+    }
+}
+
+int UART_Recieve_data()
+{
+	while(!(UCSRA) & (1<<RXC));
+    
+	return 1 ;	
+}
+
+int main(void) {
+    while (1) {
+       
+    }
+}
